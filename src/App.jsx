81cb620f4e915279b7f721b85fc6ec5e925650e9
Vince Salapuddin <vsalapuddin@ufl.edu>
2023-09-15 09:16:04 -0400

    working spotify api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
-import { useState } from "react";
-
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const CLIENT_ID = "e257d4993db149c58a1214001626e4ee";
+  const REDIRECT_URI = "http://localhost:5173/callback";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const fetchTopTracks = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get(
+      "https://api.spotify.com/v1/me/top/tracks",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {},
+      }
+    );
+
+    setTracks(data.items);
+    console.log(tracks);
+  };
+
   return (
-    <>
-      <h1>Spotify Album</h1>
-      <div className="card">
-        <a onClick={() => setCount((count) => count + 1)}>Spotify Album</a>
-      </div>
-    </>
+    <div className="h-56 grid grid-cols-3 gap-4 content-center">
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-currently-playing+user-top-read`}
+        >
+          Login to Spotify
+        </a>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+      <button onClick={fetchTopTracks}></button>
+    </div>
   );
 }
 
